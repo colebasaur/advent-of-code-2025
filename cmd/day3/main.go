8e0bf3dc314f054eb4d966e03c5b3e53e9050292
colebasaur <troutmancole12@gmail.com>
2025-12-04 00:03:01 -0600

    day 3 part 2

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -22,28 +22,9 @@ func main() {
 		banks = append(banks, bank)
 	}
 	joltages := []int{}
-	maxDigits := 2
 	for _, bank := range banks {
-		joltage := toInt(bank[:maxDigits])
-		joltageStr := bank[:maxDigits]
-		for i := maxDigits; i < len(bank); i++ {
-			opt1 := toInt(string(bank[x]) + string(bank[i]))
-			opt2 := toInt(string(bank[y]) + string(bank[i]))
-
-			maxOpt := max(opt1, opt2)
-			if maxOpt <= joltage {
-				continue
-			}
-
-			joltage = maxOpt
-
-			if opt1 > opt2 {
-				y = i
-			} else {
-				x = i
-			}
-		}
-		joltages = append(joltages, joltage)
+		joltStr := largestNDigitSubsequence(bank, 12)
+		joltages = append(joltages, toInt(joltStr))
 	}
 
 	sum := 0
@@ -55,17 +36,34 @@ func main() {
 	// fmt.Println(joltages)
 }
 
+func largestNDigitSubsequence(s string, n int) string {
+	removeLeft := len(s) - n
+	sequence := []byte{}
+
+	// 415131 (n = 3) (drop 3)
+	// ------
+	// [4], [41], [5] (drop 1 ... dropped 4, 1)
+	// [51], [53] (drop 0 ... dropped 1)
+	// dropped is 0, return 531 as they are last n remaining digits
+	for i := range len(s) {
+		char := s[i]
+
+		// while we can still drop more characters
+		// and the last character in the sequence is less than the current character
+		for removeLeft > 0 && len(sequence) > 0 && sequence[len(sequence)-1] < char {
+			// drop the last character from the sequence
+			sequence = sequence[:len(sequence)-1]
+			removeLeft--
+		}
+
+		sequence = append(sequence, char)
+	}
+
+	// only pull first n characters if we didn't drop enough
+	return string(sequence[:n])
+}
+
 func toInt(s string) int {
 	n, _ := strconv.Atoi(s)
 	return n
-}
-
-func getMinIndex(s string) int {
-	mindex := 0
-	for i := 1; i < len(s); i++ {
-		if s[mindex] > s[i] {
-			mindex = 1
-		}
-	}
-	return mindex
 }
